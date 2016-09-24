c6eda192a50665deb9708f0bec49688e14f23fdb
Lee Richmond <lrichmond1@bloomberg.net>
2016-09-24 15:12:46 -0400

    Remove useless spec

--- spec/jsonapi_suite_spec.rb
+++ spec/jsonapi_suite_spec.rb
@@ -4,8 +4,4 @@ describe JsonapiSuite do
   it 'has a version number' do
     expect(JsonapiSuite::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
