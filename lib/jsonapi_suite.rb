168863f1dedabeae710559fb3c2e883d99f102cd
Lee Richmond <lrichmond1@bloomberg.net>
2016-09-24 15:17:48 -0400

    Remove spec autorequireing

--- lib/jsonapi_suite.rb
+++ lib/jsonapi_suite.rb
@@ -6,11 +6,6 @@ require 'jsonapi_ams_extensions'
 require 'jsonapi_swagger_helpers'
 require 'nested_attribute_reassignable'
 
-if ENV['RAILS_ENV'] == 'test'
-  require 'rspec-rails'
-  require 'jsonapi_spec_helpers'
-end
-
 require "jsonapi_suite/version"
 require "jsonapi_suite/controller_mixin"
 
