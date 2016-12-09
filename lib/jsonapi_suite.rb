95d90f83d97399910b78beab21d12950b6407be2
Lee Richmond <lrichmond1@bloomberg.net>
2016-12-09 10:51:00 -0500

    Fix requires

--- lib/jsonapi_suite.rb
+++ lib/jsonapi_suite.rb
@@ -1,11 +1,9 @@
 require 'jsonapi'
 require 'jsonapi/include_directive'
 
-require 'active_model_serializers'
 require 'strong_resources'
 require 'jsonapi_compliable'
 require 'jsonapi_errorable'
-require 'jsonapi_ams_extensions'
 require 'jsonapi_swagger_helpers'
 require 'nested_attribute_reassignable'
 
