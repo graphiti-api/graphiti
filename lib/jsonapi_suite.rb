759af38789fdfa0964a00cba1bea1fe2c44b674e
Lee Richmond <lrichmond1@bloomberg.net>
2017-05-04 15:14:51 -0400

    Remove legacy dependency

--- lib/jsonapi_suite.rb
+++ lib/jsonapi_suite.rb
@@ -3,7 +3,6 @@ require 'jsonapi/include_directive'
 require 'strong_resources'
 require 'jsonapi_compliable'
 require 'jsonapi_errorable'
-require 'nested_attribute_reassignable'
 
 require "jsonapi_suite/version"
 require "jsonapi_suite/controller_mixin"
