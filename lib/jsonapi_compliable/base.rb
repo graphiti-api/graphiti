2e9ac05fc376c9e6136376f9e27f5e428cab2b32
Lee Richmond <lrichmond1@bloomberg.net>
2017-08-18 10:45:58 -0400

    Go through deserializer when checking params
    This is because jsonapi-rb is adding a _jsonapi prefix key.

--- lib/jsonapi_compliable/base.rb
+++ lib/jsonapi_compliable/base.rb
@@ -243,7 +243,7 @@ module JsonapiCompliable
     private
 
     def force_includes?
-      not params[:data].nil?
+      not deserialized_params.data.nil?
     end
 
     def perform_render_jsonapi(opts)
