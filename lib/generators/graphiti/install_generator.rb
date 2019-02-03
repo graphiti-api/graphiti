7aa23acc19eba84d4f0b8c5fb38b66a78dcae58e
Lee Richmond <lrichmond1@bloomberg.net>
2019-02-03 11:14:02 -0500

    Update install generator for Vandal

--- lib/generators/graphiti/install_generator.rb
+++ lib/generators/graphiti/install_generator.rb
@@ -47,11 +47,20 @@ RUBY
       end
 
       insert_into_file "config/routes.rb", :after => "Rails.application.routes.draw do\n" do
+        if defined?(VandalUi)
+        <<-STR
+  scope path: ApplicationResource.endpoint_namespace, defaults: { format: :jsonapi } do
+    mount VandalUi::Engine, at: '/vandal'
+    # your routes go here
+  end
+        STR
+        else
         <<-STR
   scope path: ApplicationResource.endpoint_namespace, defaults: { format: :jsonapi } do
     # your routes go here
   end
         STR
+        end
       end
     end
 
