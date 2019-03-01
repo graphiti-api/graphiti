537aa14a0618d21cacb7783497794e75f70a17a2
Wade Tandy <wtandy@bloomberg.net>
2019-03-01 15:02:40 -0500

    Remove actual graphiti codebase
    We don't need graphiti code here, just the docs

--- _site/lib/graphiti/sideload/has_one.rb
+++
@@ -1,9 +0,0 @@
-class Graphiti::Sideload::HasOne < Graphiti::Sideload::HasMany
-  def type
-    :has_one
-  end
-
-  def assign_each(parent, children)
-    children.find { |c| c.send(foreign_key) == parent.send(primary_key) }
-  end
-end
--- _site/lib/graphiti/version.rb
+++
@@ -1,3 +0,0 @@
-module Graphiti
-  VERSION = "1.0.alpha.8"
-end
