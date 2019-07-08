graphiti-api/graphiti

commit 0e2c4ddd05d5188d5620f83a687635a6c3b450ff

Allow commas for singular filters
When a filter accepts multiple values, we use a comma to delimit them.
You can escape this by adding `{{ }}`:

`?filter[foo]={{bar,baz}}`

Would treat as string "bar,baz". However, singular filters should have
this behavior automatically, because we know they are never arrays.

lib/graphiti/scoping/filter.rb
@@ -157,6 +157,8 @@ module Graphiti
     # JSON of
     # {{{ "id": 1 }}} becomes { 'id' => 1 }
     def parse_string_value(filter, value)
+      return value if !!filter[:single]
+
       type = Graphiti::Types[filter[:type]]
       array_or_string = [:string, :array].include?(type[:canonical_name])
       if (arr = value.scan(/\[.*?\]/)).present? && array_or_string

spec/filtering_spec.rb
@@ -126,6 +126,18 @@ RSpec.describe 'filtering' do
     end
   end
 
+  context 'when passed comma, but filter marked single: true' do
+    before do
+      resource.filter :first_name, single: true
+      employee2.update_attributes(first_name: 'foo,bar')
+      params[:filter] = { first_name: 'foo,bar' }
+    end
+
+    it 'does not parse as array' do
+      expect(records.map(&:id)).to eq([employee2.id])
+    end
+  end
+
   # Legacy Compat
   context 'when filter value is {{{escaped json string}}}' do
     before do
@@ -268,13 +280,6 @@ RSpec.describe 'filtering' do
           scope
         end
       end
-    end
-
-    it 'rejects multiples' do
-      params[:filter] = { first_name: { eq: 'William,Harold' } }
-      expect {
-        records
-      }.to raise_error(Graphiti::Errors::SingularFilter, /passed multiple values to filter :first_name/)
     end
 
     it 'allows singles' do

commit 0e123713a550ded58498647883d115c2a0363de0

Properly escape singular filters
0e2c4ddd05d5188d5 caused singular filters to avoid parsing the comma
into an array. However, it accidentally also prevented escaping special
characters. As a side effect, a `single: true` filter wrapped in `{{}}`
curlies would incorrectly see the value passed in containing `{{}}`.

This fixes things so singular filters still get parsed correctly, they
just avoid the comma/array scenario as originally intended.

lib/graphiti/scoping/filter.rb
@@ -151,8 +151,6 @@ module Graphiti
     # JSON of
     # {{{ "id": 1 }}} becomes { 'id' => 1 }
     def parse_string_value(filter, value)
-      return value if !!filter[:single]
-
       type = Graphiti::Types[filter[:type]]
       array_or_string = [:string, :array].include?(type[:canonical_name])
       if (arr = value.scan(/\[.*?\]/)).present? && array_or_string
@@ -165,12 +163,12 @@ module Graphiti
         }
         value = value[0] if value.length == 1
       else
-        value = parse_string_arrays(value)
+        value = parse_string_arrays(value, !!filter[:single])
       end
       value
     end
 
-    def parse_string_arrays(value)
+    def parse_string_arrays(value, singular_filter)
       # Find the quoted strings
       quotes = value.scan(/{{.*?}}/)
       # remove them from the rest
@@ -178,7 +176,11 @@ module Graphiti
       # remove the quote characters from the quoted strings
       quotes.each { |q| q.gsub!("{{", "").gsub!("}}", "") }
       # merge everything back together into an array
-      value = Array(value.split(",")) + quotes
+      if singular_filter
+        value = Array(value) + quotes
+      else
+        value = Array(value.split(",")) + quotes
+      end
       # remove any blanks that are left
       value.reject! { |v| v.length.zero? }
       value = value[0] if value.length == 1

spec/filtering_spec.rb
@@ -200,6 +200,16 @@ RSpec.describe "filtering" do
         expect(records.map(&:id)).to eq([employee2.id])
       end
     end
+
+    context 'and the filter is marked single: true' do
+      before do
+        resource.filter :first_name, :string, single: true
+      end
+
+      it 'works' do
+        expect(records.map(&:id)).to eq([employee2.id])
+      end
+    end
   end
 
   context "when passed null and filter marked allow_nil: true" do