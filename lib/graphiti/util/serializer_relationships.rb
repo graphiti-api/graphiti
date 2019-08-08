graphiti-api/graphiti

commit ef7fa514279b4da6807e5cf554e06988a58bdb52

[standardrb] Fix Lint/UnderscorePrefixedVariableName errors

lib/graphiti/util/serializer_relationships.rb
@@ -44,23 +44,23 @@ module Graphiti
       private
 
       def block
-        _link = link?
-        _resource_class = @resource_class
-        _sl = @sideload
-        _data_proc = data_proc
-        _self = self
+        link_ref = link?
+        resource_class_ref = @resource_class
+        sideload_ref = @sideload
+        data_proc_ref = data_proc
+        self_ref = self
         validate_link! if eagerly_validate_links?
 
         proc do
-          data { instance_eval(&_data_proc) }
+          data { instance_eval(&data_proc_ref) }
 
-          if _link
-            if (_links = @proxy.query.links?)
-              _self.send(:validate_link!) unless _self.send(:eagerly_validate_links?)
+          if link_ref
+            if (links_ref = @proxy.query.links?)
+              self_ref.send(:validate_link!) unless self_ref.send(:eagerly_validate_links?)
 
               link(:related) do
-                if _links
-                  ::Graphiti::Util::Link.new(_sl, @object).generate
+                if self_ref
+                  ::Graphiti::Util::Link.new(sideload_ref, @object).generate
                 end
               end
             end
@@ -69,13 +69,13 @@ module Graphiti
       end
 
       def data_proc
-        _sl = @sideload
+        sideload_ref = @sideload
         ->(_) {
-          if (records = @object.public_send(_sl.association_name))
+          if (records = @object.public_send(sideload_ref.association_name))
             if records.respond_to?(:to_ary)
-              records.each { |r| _sl.resource.decorate_record(r) }
+              records.each { |r| sideload_ref.resource.decorate_record(r) }
             else
-              _sl.resource.decorate_record(records)
+              sideload_ref.resource.decorate_record(records)
             end
 
             records
@@ -102,14 +102,14 @@ module Graphiti
 
         if @sideload.type == :polymorphic_belongs_to
           @sideload.children.each_pair do |name, sideload|
-            _validate_link!(sideload)
+            validate_link_for_sideload!(sideload)
           end
         else
-          _validate_link!(@sideload)
+          validate_link_for_sideload!(@sideload)
         end
       end
 
-      def _validate_link!(sideload)
+      def validate_link_for_sideload!(sideload)
         action = sideload.type == :belongs_to ? :show : :index
         cache_key = :"#{@sideload.object_id}-#{action}"
         return if self.class.validated_link_cache.include?(cache_key)

.standard.yml
@@ -1,0 +1,7 @@
+fix: true
+parallel: true
+
+ignore:
+  - 'vendor/**/*'
+  - 'spec/**/*':
+    - Lint/UnderscorePrefixedVariableName

lib/graphiti/hash_renderer.rb
@@ -2,8 +2,8 @@ module Graphiti
   module SerializableHash
     def to_hash(fields: nil, include: {})
       {}.tap do |hash|
-        _fields = fields[jsonapi_type] if fields
-        attrs = requested_attributes(_fields).each_with_object({}) { |(k, v), h|
+        fields_list = fields[jsonapi_type] if fields
+        attrs = requested_attributes(fields_list).each_with_object({}) { |(k, v), h|
           h[k] = instance_eval(&v)
         }
         rels = @_relationships.select { |k, v| !!include[k] }

lib/graphiti/query.rb
@@ -90,9 +90,9 @@ module Graphiti
 
             if sideload || @resource.remote?
               sl_resource = resource_for_sideload(sideload)
-              _parents = parents + [self]
+              query_parents = parents + [self]
               sub_hash = sub_hash[:include] if sub_hash.key?(:include)
-              hash[key] = Query.new(sl_resource, @params, key, sub_hash, _parents)
+              hash[key] = Query.new(sl_resource, @params, key, sub_hash, query_parents)
             else
               handle_missing_sideload(key)
             end

lib/graphiti/resource/links.rb
@@ -68,18 +68,18 @@ module Graphiti
         ([endpoint] + secondary_endpoints).compact
       end
 
-      def allow_request?(path, params, action)
-        path = path.split(".")[0]
+      def allow_request?(request_path, params, action)
+        request_path = request_path.split(".")[0]
 
         endpoints.any? do |e|
           has_id = params[:id] || params[:data].try(:[], :id)
-          _path = path
+          path = request_path
           if [:update, :show, :destroy].include?(context_namespace) && has_id
-            _path = path.split("/")
-            _path.pop
-            _path = _path.join("/")
+            path = request_path.split("/")
+            path.pop
+            path = path.join("/")
           end
-          e[:full_path].to_s == _path && e[:actions].include?(context_namespace)
+          e[:full_path].to_s == path && e[:actions].include?(context_namespace)
         end
       end
 

lib/graphiti/resource/sideloading.rb
@@ -65,10 +65,10 @@ module Graphiti
           as = opts.delete(:as)
           opts[:foreign_key] ||= :"#{as}_id"
           opts[:polymorphic_as] ||= as
-          _model = model
+          model_ref = model
           has_many name, opts do
             params do |hash|
-              hash[:filter][:"#{as}_type"] = _model.name
+              hash[:filter][:"#{as}_type"] = model_ref.name
             end
 
             instance_eval(&blk) if block_given?

lib/graphiti/scope.rb
@@ -40,11 +40,11 @@ module Graphiti
       @query.sideloads.each_pair do |name, q|
         sideload = @resource.class.sideload(name)
         next if sideload.nil? || sideload.shared_remote?
-        _parent = @resource
-        _context = Graphiti.context
+        parent_resource = @resource
+        graphiti_context = Graphiti.context
         resolve_sideload = -> {
-          Graphiti.context = _context
-          sideload.resolve(results, q, _parent)
+          Graphiti.context = graphiti_context
+          sideload.resolve(results, q, parent_resource)
           if concurrent && defined?(ActiveRecord)
             ActiveRecord::Base.clear_active_connections!
           end

lib/graphiti/sideload.rb
@@ -81,11 +81,11 @@ module Graphiti
     end
 
     def create_remote_resource
-      _remote = @remote
+      remote_url = @remote
       klass = Class.new(Graphiti::Resource) {
         self.adapter = Graphiti::Adapters::GraphitiAPI
         self.model = OpenStruct
-        self.remote = _remote
+        self.remote = remote_url
         self.validate_endpoints = false
       }
       name = "#{parent_resource_class.name}.#{@name}.remote"

commit 338ce7e60591be7fb2ada03c186fcf54d940e7e6

Remove unnecessary (and incorrect) conditional
Introduced in ef7fa514279b4da6807e5cf554e06988a58bdb52

lib/graphiti/util/serializer_relationships.rb
@@ -58,9 +58,7 @@ module Graphiti
               self_ref.send(:validate_link!) unless self_ref.send(:eagerly_validate_links?)
 
               link(:related) do
-                if self_ref
-                  ::Graphiti::Util::Link.new(sideload_ref, @object).generate
-                end
+                ::Graphiti::Util::Link.new(sideload_ref, @object).generate
               end
             end
           end