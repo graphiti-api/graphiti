cdb749ee90f32aa9f328d8fdc65c867e477196ca
Seosamh Cahill <seo.cahill@gmail.com>
2020-06-02 12:35:00 +0200

    rubocop

--- lib/graphiti.rb
+++ lib/graphiti.rb
@@ -33,7 +33,7 @@ module Graphiti
     resources.each do |resource_class|
       resource_class.sideloads.values.each do |sideload|
         flag = sideload.instance_variable_get(:@readable)
-        next if evaluated = sideload.send(:evaluate_flag, flag)
+        next if (evaluated = sideload.send(:evaluate_flag, flag))
 
         resource_class.serializer.relationship(sideload.name, if: -> { evaluated })
       end
