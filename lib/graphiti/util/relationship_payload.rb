10cde1e6fc93d40b88fe02e5ea85e69f7df6ceb4
Samuel J Clopton <samsinite@gmail.com>
2020-12-18 13:25:02 -0800

    ...

--- lib/graphiti/util/relationship_payload.rb
+++ lib/graphiti/util/relationship_payload.rb
@@ -70,31 +70,46 @@ module Graphiti
       end
 
       def payload_for(sideload, relationship_payload)
-        type = relationship_payload[:meta][:jsonapi_type].to_sym
+        if relationship_payload[:meta][:method] == :nullify
+          resource = sideload.resource
 
-        # For polymorphic *sideloads*, grab the correct child sideload
-        if sideload.resource.type != type && sideload.type == :polymorphic_belongs_to
-          sideload = sideload.child_for_type!(type)
+          {
+            resource: resource,
+            sideload: sideload,
+            is_polymorphic: sideload.polymorphic_child?,
+            primary_key: sideload.primary_key,
+            foreign_key: sideload.foreign_key,
+            attributes: relationship_payload[:attributes],
+            meta: relationship_payload[:meta],
+            relationships: relationship_payload[:relationships]
+          }
+        else
+          type = relationship_payload[:meta][:jsonapi_type].to_sym
+
+          # For polymorphic *sideloads*, grab the correct child sideload
+          if sideload.resource.type != type && sideload.type == :polymorphic_belongs_to
+            sideload = sideload.child_for_type!(type)
+          end
+
+          # For polymorphic *resources*, grab the correct child resource
+          resource = sideload.resource
+          if resource.type != type && resource.polymorphic?
+            resource = resource.class.resource_for_type(type).new
+          end
+
+          relationship_payload[:meta][:method] ||= :update
+
+          {
+            resource: resource,
+            sideload: sideload,
+            is_polymorphic: sideload.polymorphic_child?,
+            primary_key: sideload.primary_key,
+            foreign_key: sideload.foreign_key,
+            attributes: relationship_payload[:attributes],
+            meta: relationship_payload[:meta],
+            relationships: relationship_payload[:relationships]
+          }
         end
-
-        # For polymorphic *resources*, grab the correct child resource
-        resource = sideload.resource
-        if resource.type != type && resource.polymorphic?
-          resource = resource.class.resource_for_type(type).new
-        end
-
-        relationship_payload[:meta][:method] ||= :update
-
-        {
-          resource: resource,
-          sideload: sideload,
-          is_polymorphic: sideload.polymorphic_child?,
-          primary_key: sideload.primary_key,
-          foreign_key: sideload.foreign_key,
-          attributes: relationship_payload[:attributes],
-          meta: relationship_payload[:meta],
-          relationships: relationship_payload[:relationships]
-        }
       end
     end
   end
