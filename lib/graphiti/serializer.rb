096eb064a60c08cebd3a17b85d00e69954fb2f1c
Lee Richmond <lrichmond1@bloomberg.net>
2021-02-26 12:39:49 -0500

    Allow same name for attribute and relationship
    This was already possible unless the attribute was guarded, in which
    case the relationship would incorrectly share the same guard. This
    is a "bug" in jsonapi-rb, so we add to our existing hacks on top of
    that library pending an upstream fix.
    
    In practice this is helpful for a GQL-specific federated attribute,
    which would share the same name as a relationship but be conditionally
    rendered only for GQL.

--- lib/graphiti/serializer.rb
+++ lib/graphiti/serializer.rb
@@ -12,13 +12,36 @@ module Graphiti
     # go through type checking/coercion
     class_attribute :attributes_applied_via_resource
     class_attribute :extra_attributes_applied_via_resource
+    class_attribute :relationship_condition_blocks
     self.attributes_applied_via_resource = []
     self.extra_attributes_applied_via_resource = []
+    # See #requested_relationships
+    self.relationship_condition_blocks ||= {}
 
     def self.inherited(klass)
       super
       klass.class_eval do
         extend JSONAPI::Serializable::Resource::ConditionalFields
+
+        # See #requested_relationships
+        def self.relationship(name, options = {}, &block)
+          super
+          field_condition_blocks.delete(name)
+          _register_condition(relationship_condition_blocks, name, options)
+        end
+
+        # NB - avoid clobbering includes when sparse fieldset
+        # https://github.com/jsonapi-rb/jsonapi-serializable/pull/102
+        #
+        # We also override this method to ensure attributes and relationships
+        # have separate condition blocks. This way an attribute and
+        # relationship can have the same name, and the attribute can be
+        # conditional without affecting the relationship.
+        def requested_relationships(fields)
+          @_relationships.select do |k, _|
+            _conditionally_included?(self.class.relationship_condition_blocks, k)
+          end
+        end
       end
     end
 
@@ -27,12 +50,6 @@ module Graphiti
         strip_relationships!(hash) if strip_relationships?
         add_links!(hash)
       end
-    end
-
-    # Temporary fix until fixed upstream
-    # https://github.com/jsonapi-rb/jsonapi-serializable/pull/102
-    def requested_relationships(fields)
-      @_relationships
     end
 
     # Allow access to resource methods
