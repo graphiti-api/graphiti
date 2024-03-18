e1dd811283f6e6fe7a36b925934df0ecbb4d3411
doublevoid <gabrielgarocs@gmail.com>
2024-03-18 14:04:45 -0300

    fix: prevent :id stripping when :id not in path (#447)
    Address edge case where ResourceName.find({id: id}) would error if controller was set up as a show action

--- lib/graphiti/resource/links.rb
+++ lib/graphiti/resource/links.rb
@@ -76,7 +76,7 @@ module Graphiti
           path = request_path
           if [:update, :show, :destroy].include?(context_namespace) && has_id
             path = request_path.split("/")
-            path.pop
+            path.pop if path.last == has_id.to_s
             path = path.join("/")
           end
           e[:full_path].to_s == path && e[:actions].include?(context_namespace)
