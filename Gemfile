309b980358a03d8f0496f5dd755693f57e05fd19
Matt Fenelon <matt.fenelon@carwow.co.uk>
2024-05-22 11:02:29 +0100

    Make a gemfile that works locally without appraisal
    To debug the rails tests. I'll remove this once the tests are passing.

--- Gemfile
+++ Gemfile
@@ -1,13 +1,22 @@
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
 # Specify your gem's dependencies in graphiti.gemspec
 gemspec
 
+github 'rails/rails' do
+  gem 'activemodel'
+  gem 'rails'
+end
+
+gem 'database_cleaner'
+gem 'graphiti-rails', '~> 0.4.0'
+gem 'rspec-rails'
+gem 'sqlite3'
+
 group :test do
-  gem "database_cleaner"
-  gem "pry"
-  gem "pry-byebug", platform: [:mri]
-  gem "appraisal"
-  gem "guard"
-  gem "guard-rspec"
+  gem 'appraisal'
+  gem 'guard'
+  gem 'guard-rspec'
+  gem 'pry'
+  gem 'pry-byebug', platform: [:mri]
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,19 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 
-Dir[File.dirname(__FILE__) + "/support/**/*.rb"].sort.each { |f| require f }
-require "pry"
+Dir[File.dirname(__FILE__) + '/support/**/*.rb'].sort.each { |f| require f }
+require 'pry'
 
-require "active_model"
-require "graphiti_spec_helpers/rspec"
-require "graphiti"
+require 'active_model'
+require 'graphiti_spec_helpers/rspec'
+require 'graphiti'
 # Avoiding loading classes before we're ready
 Graphiti::Resource.autolink = false
-require "fixtures/poro"
+require 'fixtures/poro'
 Graphiti.setup!
 
 # Optional dep for cross-api requests
-require "faraday"
-require "base64"
+require 'faraday'
+require 'base64'
 
 RSpec.configure do |config|
   config.include GraphitiSpecHelpers::RSpec
@@ -36,31 +36,35 @@ RSpec.configure do |config|
 
   config.filter_run_when_matching :focus
 
-  config.example_status_persistence_file_path = File.expand_path(".rspec-examples", __dir__)
+  config.example_status_persistence_file_path = File.expand_path('.rspec-examples', __dir__)
 end
 
 # We test rails through appraisal
-if ENV["APPRAISAL_INITIALIZED"]
+if ENV['APPRAISAL_INITIALIZED']
   RSpec.configure do |config|
     # If not running tests for specific file, only run rails tests
-    if config.instance_variable_get(:@files_or_directories_to_run) == ["spec"]
-      config.pattern = "spec/integration/rails/**/*_spec.rb"
+    if config.instance_variable_get(:@files_or_directories_to_run) == ['spec']
+      config.pattern = 'spec/integration/rails/**/*_spec.rb'
     end
   end
 
   # Avoid checking, because Rails is defined but we dont have autoloading
   Graphiti::Sideload.class_eval do
-    def check!
-    end
+    def check!; end
   end
 
-  require "database_cleaner"
-  require "kaminari"
-  require "active_record"
-  require "graphiti/adapters/active_record"
-  require "rails_spec_helper"
+  require 'database_cleaner'
+  require 'kaminari'
+  require 'active_record'
+  require 'graphiti/adapters/active_record'
+  require 'rails_spec_helper'
+  class DatabaseCleaner::ActiveRecord::Base
+    def self.migration_table_name
+      ::ActiveRecord::Base.connection_pool.schema_migration.table_name
+    end
+  end
   ActiveRecord::Migration.verbose = false
-  ActiveRecord::Base.establish_connection adapter: "sqlite3",
-    database: ":memory:"
-  Dir[File.dirname(__FILE__) + "/fixtures/**/*.rb"].sort.each { |f| require f }
+  ActiveRecord::Base.establish_connection adapter: 'sqlite3',
+                                          database: ':memory:'
+  Dir[File.dirname(__FILE__) + '/fixtures/**/*.rb'].sort.each { |f| require f }
 end
