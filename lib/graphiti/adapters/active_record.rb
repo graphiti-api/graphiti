graphiti-api/graphiti

commit 8313e3359f0dde28d9940867c7ded964db4c854d

fix: update ActiveRecord adapter w/ support for Rails 7.2+ (#478)
In ActiveRecord 7.2 the `clear_active_connections` was (re)moved to the `connection_handler` property.

lib/graphiti/adapters/active_record.rb
@@ -304,7 +304,11 @@ module Graphiti
       end
 
       def close
-        ::ActiveRecord::Base.clear_active_connections!
+        if ::ActiveRecord.version > 7.2
+          ::ActiveRecord::Base.connection_handler.clear_active_connections!
+        else
+          ::ActiveRecord::Base.clear_active_connections!
+        end
       end
 
       def can_group?

commit 42c82c397f20eb91c02835e518ff4c351c028ea7

fix: Fixes error in version check for ActiveRecord adapter introduced in #478 (#479)

lib/graphiti/adapters/active_record.rb
@@ -304,7 +304,7 @@ module Graphiti
       end
 
       def close
-        if ::ActiveRecord.version > 7.2
+        if ::ActiveRecord.version > "7.2"
           ::ActiveRecord::Base.connection_handler.clear_active_connections!
         else
           ::ActiveRecord::Base.clear_active_connections!