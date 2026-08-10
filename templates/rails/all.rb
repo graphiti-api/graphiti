bd113ff369a5fb1f61ff25d76221d89aaf90e394
Jeff Keen <jeff@keen.me>
2026-08-10 12:38:33 -0500

    chore: lint the Rails app template, exempting only the heredoc cops

--- templates/rails/all.rb
+++ templates/rails/all.rb
@@ -1,17 +1,17 @@
 Thor::Base.shell = Thor::Shell::Color
-require 'yaml'
+require "yaml"
 
 def update_config!(attrs)
-  config = File.exist?('.graphiticfg.yml') ? YAML.load_file('.graphiticfg.yml') : {}
+  config = File.exist?(".graphiticfg.yml") ? YAML.load_file(".graphiticfg.yml") : {}
   config.merge!(attrs)
-  File.open('.graphiticfg.yml', 'w') { |f| f.write(config.to_yaml) }
+  File.write(".graphiticfg.yml", config.to_yaml)
 end
 
 def prompt(header: nil, description: nil, default: nil)
   say(set_color("\n#{header}", :magenta, :bold)) if header
   say("\n#{description}") if description
   answer = ask(set_color("\n(default: #{default}):", :magenta, :bold))
-  answer = default if answer.blank? && default != 'nil'
+  answer = default if answer.blank? && default != "nil"
   say(set_color("\nGot it!\n", :white, :bold))
   answer
 end
@@ -21,8 +21,8 @@ def api_namespace
     ns = prompt \
       header: "What is your API namespace?",
       description: "This will be used as a route prefix, e.g. if you want the route '/books_api/v1/authors' your namespace would be '/books_api/v1'",
-      default: '/api/v1'
-    update_config!('namespace' => ns)
+      default: "/api/v1"
+    update_config!("namespace" => ns)
     ns
   end
 end
@@ -40,18 +40,18 @@ STR
 say(set_color(welcome.rstrip, :cyan, :bold))
 api_namespace
 
-gem 'graphiti', '~> 2.0.0.beta'
-gem 'vandal_ui'
-gem 'kaminari'
+gem "graphiti", "~> 2.0.0.beta"
+gem "vandal_ui"
+gem "kaminari"
 
 gem_group :development, :test do
-  gem 'rspec-rails'
-  gem 'factory_bot_rails'
-  gem 'faker' # keep here for seeds.rb
+  gem "rspec-rails"
+  gem "factory_bot_rails"
+  gem "faker" # keep here for seeds.rb
 end
 
 gem_group :test do
-  gem 'database_cleaner-active_record', '~> 2.2'
+  gem "database_cleaner-active_record", "~> 2.2"
 end
 
 after_bundle do
